91ac82d9e0fb9092f73779e4a68897b57292ebef
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2021-01-27 10:43:49 +0300

    Update;- minor changes to commit

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ const rateLimit = require("express-rate-limit");
 const {getFirebaseUser} = require('./helpers/firebaseSecurity');
 
 
+
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100 // limit each IP to 100 requests per windowMs
